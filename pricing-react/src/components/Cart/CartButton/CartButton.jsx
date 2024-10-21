be03ec7f042d7831c92f6f3b21917decd9f24b66
Zibait-it <lucas.zibaitis@lightit.io>
2024-10-21 20:20:46 -0300

    feat: add animations to modals and buttons

--- pricing-react/src/components/Cart/CartButton/CartButton.jsx
+++ pricing-react/src/components/Cart/CartButton/CartButton.jsx
@@ -1,7 +1,7 @@
 import { PricingIcon } from "../../icons/pricingIcon/PricingIcon";
 import EmptyCart from "../emptyCart/EmptyCart";
 import FilledCart from "../filledCart/FilledCart";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useCartStore } from "../../../store/cartStore";
 
 import styles from "./CartButton.module.scss";
@@ -9,13 +9,29 @@ import styles from "./CartButton.module.scss";
 export default function CartButton() {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const { items, total } = useCartStore((state) => state.cart);
+  const mainContainerRef = useRef(null);
 
+  useEffect(() => {
+    if (items.length > 0) {
+      // Añade la clase para la animación
+      const mainContainer = mainContainerRef.current;
+      mainContainer.classList.add(styles.expand);
+
+      // Elimina la clase después de 500ms (duración de la animación)
+      const timer = setTimeout(() => {
+        mainContainer.classList.remove(styles.expand);
+      }, 500);
+
+      // Limpieza
+      return () => clearTimeout(timer);
+    }
+  }, [items.length]); //
   const handleClick = () => {
     setIsCartOpen(!isCartOpen);
   };
 
   return (
-    <div className={styles["main-container"]}>
+    <div ref={mainContainerRef} className={`${styles["main-container"]}`}>
       <button className={styles.button} onClick={handleClick}>
         <PricingIcon className={styles["pricing-icon"]} />
         {items.length > 0 && (
